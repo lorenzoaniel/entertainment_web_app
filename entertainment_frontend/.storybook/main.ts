b96062007dc27ae1f9c9acbbd9e539da8d5c3395
lorenzoaniel <lorenzoaniel@gmail.com>
2023-08-02 19:00:31 -0600

    Storybook setup

--- entertainment_frontend/.storybook/main.ts
+++ entertainment_frontend/.storybook/main.ts
@@ -1,19 +1,35 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: [
+		// "../src/**/*.mdx",
+		"../src/app/**/*.stories.@(js|jsx|ts|tsx)",
+		"../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/nextjs",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	// need this for assets
+	staticDirs: ["../public", "../src"],
+	// so that zustand store works in storybook
+	webpackFinal: async (config, { configType }) => {
+		config.resolve = config.resolve ?? {};
+		config.resolve.alias = {
+			...config.resolve.alias,
+			"@": path.resolve(__dirname, "../src"),
+		};
+
+		return config;
+	},
 };
 export default config;
