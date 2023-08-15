80936c4c66cadf642726d74a1e79a55ee0a19f8c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-08-14 21:28:41 -0600

    Update Card.stories.tsx

--- entertainment_frontend/src/components/card/Card.stories.tsx
+++ entertainment_frontend/src/components/card/Card.stories.tsx
@@ -18,17 +18,23 @@ export default meta;
 export const PrimaryTrending: Story = {
 	args: {
 		cardProps: {
+			title: "Beyond Earth",
+			thumbnail: {
+				trending: {
+					small: "./assets/thumbnails/beyond-earth/trending/small.jpg",
+					large: "./assets/thumbnails/beyond-earth/trending/large.jpg",
+				},
+				regular: {
+					small: "./assets/thumbnails/beyond-earth/regular/small.jpg",
+					medium: "./assets/thumbnails/beyond-earth/regular/medium.jpg",
+					large: "./assets/thumbnails/beyond-earth/regular/large.jpg",
+				},
+			},
+			year: 2019,
 			category: "Movie",
-			year: 2023,
-			title: "Trending Title Test",
 			rating: "PG",
 			isBookmarked: false,
 			isTrending: true,
-			thumbnailSrc: {
-				small: "./assets/thumbnails/beyond-earth/trending/small.jpg",
-				medium: "./assets/thumbnails/beyond-earth/trending/medium.jpg",
-				large: "./assets/thumbnails/beyond-earth/trending/large.jpg",
-			},
 		},
 	},
 };
@@ -36,17 +42,23 @@ export const PrimaryTrending: Story = {
 export const PrimaryNonTrending: Story = {
 	args: {
 		cardProps: {
+			title: "Beyond Earth",
+			thumbnail: {
+				trending: {
+					small: "./assets/thumbnails/beyond-earth/trending/small.jpg",
+					large: "./assets/thumbnails/beyond-earth/trending/large.jpg",
+				},
+				regular: {
+					small: "./assets/thumbnails/beyond-earth/regular/small.jpg",
+					medium: "./assets/thumbnails/beyond-earth/regular/medium.jpg",
+					large: "./assets/thumbnails/beyond-earth/regular/large.jpg",
+				},
+			},
+			year: 2019,
 			category: "Movie",
-			year: 2023,
-			title: "Community of Ours",
 			rating: "PG",
 			isBookmarked: false,
 			isTrending: false,
-			thumbnailSrc: {
-				small: "./assets/thumbnails/the-great-lands/regular/small.jpg",
-				medium: "./assets/thumbnails/the-great-lands/regular/medium.jpg",
-				large: "./assets/thumbnails/the-great-lands/regular/large.jpg",
-			},
 		},
 	},
 };
