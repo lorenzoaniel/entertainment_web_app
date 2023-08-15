0833ae4e68bf3856d37f3457eeb5d7c342ab092a
lorenzoaniel <lorenzoaniel@gmail.com>
2023-08-14 21:28:29 -0600

    Update page.tsx

--- entertainment_frontend/src/app/loggedin/page.tsx
+++ entertainment_frontend/src/app/loggedin/page.tsx
@@ -5,18 +5,21 @@ import React from "react";
 
 export default function LoggedIn({ children }: { children: React.ReactNode }) {
 	return (
-		<section
+		<main
 			className={clsx(
 				"loggedin-home",
 				`
           w-full h-full
-          bg-transparent 
+          bg-transparent
+					flex flex-col lg:grid
+					lg:grid-cols-[13rem_1fr] lg:grid-rows-1
+					gap-y-[2.6rem] md:gap-y-[3.4rem] lg:gap-y-0
         `
 			)}
 		>
 			<Menu />
 			<Searchbar />
 			{children}
-		</section>
+		</main>
 	);
 }
