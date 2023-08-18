3d224620adafa0887c014a00643a35e51ecb76b1
lorenzoaniel <lorenzoaniel@gmail.com>
2023-08-18 15:18:34 -0600

    Create _input_mixins.ts

---
+++ entertainment_frontend/src/style/mixins/_input_mixins.ts
@@ -0,0 +1,11 @@
+export const _input_mixins = {
+	formInputs: `
+		w-full h-[3.7rem]
+		bg-transparent
+		border-b-[0.1rem] focus:border-white-pure 
+		pl-[1.6rem]
+		
+		outline-none
+		text-body-m text-white-pure
+	`,
+};
